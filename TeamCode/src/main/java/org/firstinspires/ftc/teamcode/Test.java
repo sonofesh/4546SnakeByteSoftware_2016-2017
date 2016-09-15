9e406c63999549ee60767e646b8b1140a007eabb
sonofesh <r3spatel@gmail.com>
2016-09-14 19:04:56 -0500

    updated ftc app

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by sopa on 9/13/16.
+ */
+@TeleOp(name = "Test", group = "Teleop")
+public class Test extends OpMode{
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}

336f449c7087f3b0250d66c7130cbca43b905794
sonofesh <r3spatel@gmail.com>
2016-09-20 16:38:07 -0500

    init method in autoopmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoOpMode.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.robotcontroller.external.samples.SensorAdafruitIMU;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 /**
  * Created by sopa on 9/19/16.
@@ -26,12 +27,29 @@ public class AutoOpMode extends LinearOpMode
     int BRV, FRV;
     int avg;
 
+    public void initialize(String side) throws InterruptedException
+    {
+        FR = hardwareMap.dcMotor.get("FR");
+        BR = hardwareMap.dcMotor.get("BR");
+        FL = hardwareMap.dcMotor.get("FL");
+        BL = hardwareMap.dcMotor.get("BL");
+        FL.setPower(0);
+        FR.setPower(0);
+        BL.setPower(0);
+        BR.setPower(0);
+        final Telemetry.Item item = telemetry.addData("gyro init, beggining");
+
+
+    }
 
     @Override
     public void runOpMode() throws InterruptedException
     {
         
 
+
+    }
+    public void moveForwardWithEncoders(){
 
     }
 }

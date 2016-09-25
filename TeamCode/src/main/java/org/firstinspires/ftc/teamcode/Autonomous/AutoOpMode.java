55b85da3c8fb0d1127baac93db1df318b9daff8b
sonofesh <r3spatel@gmail.com>
2016-09-25 15:45:46 -0500

    Added Encoder methods and gyro init to the AutoOpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoOpMode.java
@@ -2,6 +2,9 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 
 import com.qualcomm.hardware.adafruit.AdafruitI2cColorSensor;
 import com.qualcomm.hardware.adafruit.BNO055IMU;
+import com.qualcomm.hardware.adafruit.JustLoggingAccelerationIntegrator;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -14,7 +17,7 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 /**
  * Created by sopa on 9/19/16.
  */
-@TeleOp(name = "AutoOpMode", group = "Autonomous")
+@Autonomous(name = "AutoOpMode", group = "Autonomous")
 @Disabled
 public abstract class AutoOpMode extends LinearOpMode
 {
@@ -22,7 +25,7 @@ public abstract class AutoOpMode extends LinearOpMode
     DcMotor BR;
     DcMotor FL;
     DcMotor BL;
-    SensorAdafruitIMU IMU;
+    BNO055IMU imu;;
     ColorSensor colorSensor1;
     ColorSensor colorSensor2;
     int BRV, BLV;
@@ -33,7 +36,7 @@ public abstract class AutoOpMode extends LinearOpMode
         super();
     }
 
-    public void initialize(String side) throws InterruptedException
+    public void initialize() throws InterruptedException
     {
         FR = hardwareMap.dcMotor.get("FR");
         BR = hardwareMap.dcMotor.get("BR");
@@ -44,9 +47,25 @@ public abstract class AutoOpMode extends LinearOpMode
         BL.setPower(0);
         BR.setPower(0);
         telemetry.addData("gyro", "initializing");
-        //initalize sensors
-        IMU = new SensorAdafruitIMU();
-        //gyro init
+        //Initialize Sensors
+        //Color Sensor Initialization
+        //set up parameters for gyro sensors
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "AdafruitIMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+        //Color Sensor Initialization
+
+        BL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BRV = 0;
+        BLV = 0;
+        reset();
     }
 
     //Basic base movement
@@ -72,18 +91,48 @@ public abstract class AutoOpMode extends LinearOpMode
     {
         turnRight(-power);
     }
-
-    //Methods based soley on encoders
-    public void getAvg() throws InterruptedException {
-        sleep(500);
+    public void stopBase()
+    {
+        FR.setPower(0);
+        BR.setPower(0);
+        FL.setPower(0);
+        BL.setPower(0);
+    }
+    //Methods based solely on encoders
+    public void reset() throws InterruptedException
+    {
+        FR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BRV = 0;
+        BLV = 0;
+        stop();
+    }
+    public int getAvg() throws InterruptedException
+    {
         BRV = Math.abs(BR.getCurrentPosition());
         BLV = Math.abs(BL.getCurrentPosition());
         avg = (BRV + BLV)/2;
-
+        return avg;
     }
-    public void moveForwardWithEncoders(double power, int distance)
+    public void moveForwardWithEncoders(double power, int distance) throws InterruptedException
     {
-
+        while(getAvg() < distance)
+        {
+            moveForward(power);
+        }
+        reset();
+    }
+    public void moveBackWardWithEncoders(double power, int distance) throws InterruptedException
+    {
+        while(getAvg() < distance)
+        {
+            moveBackward(power);
+        }
+        reset();
+    }
+    //Gyro based turning methods
+    public void turnRightWithGyro()
+    {
 
     }
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TestEncoders.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by sopa on 9/24/16.
+ */
+@Autonomous(name = "TestEncoders", group = "Autonomous")
+public class TestEncoders extends AutoOpMode
+{
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        initialize();
+        waitForStart();
+        moveForwardWithEncoders(.5, 500);
+        moveBackWardWithEncoders(.5, 500);
+    }
+}

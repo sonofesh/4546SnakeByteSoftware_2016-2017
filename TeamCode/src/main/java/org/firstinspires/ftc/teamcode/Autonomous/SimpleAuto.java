b2a0261d5299ffd9aaf24fa69de3458460ddc4ca
sonofesh <r3spatel@gmail.com>
2016-11-17 15:58:05 -0600

    Changed turning methods to be gyro based

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SimpleAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SimpleAuto.java
@@ -2,9 +2,13 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 
 import android.os.SystemClock;
 
+import com.qualcomm.hardware.adafruit.BNO055IMU;
+import com.qualcomm.hardware.adafruit.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 /**
  * Created by sopa on 11/9/16.
@@ -20,13 +24,18 @@ public class SimpleAuto extends LinearOpMode
     DcMotor ShooterF;
     DcMotor ManLift;
     DcMotor ManIn;
+    //average encoder value
     int beforeALV = 0;
     int beforeMLV = 0;
+    double beforeAngle = 0;
     int FRV = 0;
     int FLV = 0;
     int avg = 0;
     long beforeTime = 0;
     long currentTime = 0;
+    public static BNO055IMU imu;
+    public static BNO055IMU.Parameters parameters;
+
     public void zero() throws InterruptedException
     {
         FR.setPower(0);
@@ -34,6 +43,7 @@ public class SimpleAuto extends LinearOpMode
         FL.setPower(0);
         BL.setPower(0);
     }
+
     public void moveForward(double power) throws InterruptedException
     {
         FR.setPower(power);
@@ -41,17 +51,32 @@ public class SimpleAuto extends LinearOpMode
         FL.setPower(-power);
         BL.setPower(-power);
     }
+
     public void moveBackward(double power) throws InterruptedException
     {
         moveForward(-power);
     }
+
     public void turnRight(double power) throws InterruptedException
     {
         FR.setPower(power);
         BR.setPower(power);
-        FL.setPower(0);
-        BL.setPower(0);
+        FL.setPower(-power);
+        BL.setPower(-power);
     }
+
+    public void turnLeft(double power) throws InterruptedException
+    {
+        turnRight(-power);
+    }
+
+    //gyro methods
+    public float getGryoYaw()
+    {
+        Orientation angles = imu.getAngularOrientation();
+        return angles.firstAngle;
+    }
+
     public void bringDownShooter(double power, int distance) throws InterruptedException
     {
         int beforePos = Math.abs(ManLift.getCurrentPosition());
@@ -66,6 +91,7 @@ public class SimpleAuto extends LinearOpMode
         telemetry.addData("ManLift", ManLift.getCurrentPosition());
         telemetry.update();
     }
+
     public void shoot(double power, int distance) throws InterruptedException
     {
         bringDownShooter(.3 * -1, distance);
@@ -73,6 +99,7 @@ public class SimpleAuto extends LinearOpMode
         ShooterF.setPower(power);
         ShooterB.setPower(-power);
     }
+
     public void shootTime(double power) throws InterruptedException
     {
         ShooterF.setPower(power);
@@ -88,10 +115,7 @@ public class SimpleAuto extends LinearOpMode
         ShooterF.setPower(0);
         ShooterB.setPower(0);
     }
-    public void turnLeft(double power) throws InterruptedException
-    {
-        turnRight(-power);
-    }
+
     public int getAvg() throws InterruptedException
     {
         FRV = Math.abs(FR.getCurrentPosition());
@@ -99,10 +123,10 @@ public class SimpleAuto extends LinearOpMode
         avg = Math.abs((FRV + FLV)/2);
         return avg;
     }
+
     @Override
     public void runOpMode() throws InterruptedException
     {
-        waitForStart();
         FR = hardwareMap.dcMotor.get("FR");
         BR = hardwareMap.dcMotor.get("BR");
         FL = hardwareMap.dcMotor.get("FL");
@@ -117,6 +141,17 @@ public class SimpleAuto extends LinearOpMode
         FR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         FL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         ManLift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "AdafruitIMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+        imu.initialize(parameters);
+        telemetry.addData("gyro", "initalized");
+        telemetry.update();
+        waitForStart();
         //beforeALV = getAvg();
         //wait(15000);
         telemetry.addData("encodersR", FR.getCurrentPosition());
@@ -140,8 +175,8 @@ public class SimpleAuto extends LinearOpMode
         //bring down shooter
         bringDownShooter(.1, 900);
         sleep(1000);
-        beforeALV = getAvg();
-        while(getAvg() <  beforeALV + 50)
+        beforeAngle = getGryoYaw();
+        while(Math.abs(getGryoYaw() - beforeALV) > 20)
         {
             turnLeft(.3);
             //telemetry.addData("distance", Math.abs(System.currentTimeMillis() - beforeTime));
@@ -152,10 +187,12 @@ public class SimpleAuto extends LinearOpMode
         BR.setPower(0);
         FL.setPower(0);
         BL.setPower(0);
-        telemetry.addData("encoders", getAvg());
+        telemetry.addData("yaw angle", getGryoYaw());
         telemetry.update();
-        sleep(3000);
+        sleep(1000);
         shoot(1, 400);
+        ShooterB.setPower(0);
+        ShooterF.setPower(0);
         //long currTime = System.currentTimeMillis();
         //if(Math.abs(currTime - startTime) > 15000)
     }
